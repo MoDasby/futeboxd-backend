eba830739401e64725c81828cb2924c60a2745dd
MoDasby <giullianomendes033@gmail.com>
2024-11-03 14:16:46 -0300

    add page size limit

--- services/core/internal/pagination/page.go
+++ services/core/internal/pagination/page.go
@@ -2,6 +2,8 @@ package pagination
 
 import (
 	"strconv"
+
+	"github.com/modasby/futeboxd-api/services/core/internal/errors"
 )
 
 type Page struct {
@@ -19,6 +21,14 @@ func WithPage(size, index string) (*Page, error) {
 	indexInt, err := strconv.ParseUint(index, 10, 64)
 	if err != nil {
 		indexInt = 1
+	}
+
+	if sizeInt > 100 {
+		return nil, errors.NewHTTPErr(
+			"tamanho da página não pode ser maior que 100",
+			400,
+			"PAGINATION:PAGE_SIZE_TOO_BIG",
+		)
 	}
 
 	return &Page{
