76a829d18ea060909a0acd2908251d2bb542fa0c
MoDasby <giullianomendes033@gmail.com>
2024-11-03 00:19:37 -0300

    fix test

--- services/core/internal/usecase/users/toggle_follow_test.go
+++ services/core/internal/usecase/users/toggle_follow_test.go
@@ -41,5 +41,5 @@ func TestToggleFollow(t *testing.T) {
 	output, err = uc.Execute(follower.ID, following.ID)
 
 	assert.NoError(t, err)
-	assert.True(t, output.Follow)
+	assert.False(t, output.Follow)
 }
