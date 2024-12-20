610ca26d6560af8bbdc055546e7825fe3be9666f
MoDasby <giullianomendes033@gmail.com>
2024-12-20 14:08:59 -0300

    add email

---
+++ core/internal/email/email.go
@@ -0,0 +1,31 @@
+package email
+
+import (
+	"fmt"
+	"net/smtp"
+)
+
+type EmailOpts struct {
+	ContentType string
+	To          string
+	Subject     string
+	Body        string
+}
+
+const (
+	from string = "giullianomendes033@gmail.com"
+	pass string = "token"
+)
+
+func SendMail(opts EmailOpts) error {
+	message := fmt.Sprintf("From: %s\r\nTo: %s\r\nSubject: %s\r\nMIME-version: 1.0;\r\nContent-Type: %s; charset=\"UTF-8\";\r\n\r\n%s", from, opts.To, opts.Subject, opts.ContentType, opts.Body)
+
+	if err := smtp.SendMail("smtp.gmail.com:587",
+		smtp.PlainAuth("futeboxd@email.com", from, pass, "smtp.gmail.com"),
+		from, []string{opts.To}, []byte(message)); err != nil {
+
+		return err
+	}
+
+	return nil
+}
