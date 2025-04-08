e5e91297749be9c28be8b34105329c3a1a86f605
MoDasby <giullianomendes033@gmail.com>
2025-04-08 10:09:03 -0300

    refactor: update logger configuration

--- football-data-updater/src/util/logger.ts
+++ football-data-updater/src/util/logger.ts
@@ -1,23 +1,23 @@
 import winston from "winston";
 import { WinstonTransport as AxiomTransport } from '@axiomhq/winston';
 
-const logger = winston.createLogger({
-    format: winston.format.json(),
-    defaultMeta: { service: 'football-data-updater' },
-    transports: [
-        new AxiomTransport({
-            dataset: "futeboxd-logs",
-            token: process.env.AXIOM_TOKEN as string
-        }),
-    ],
-});
+let transports: winston.transport[] = [];
 
-if (process.env.NODE_ENV != 'production') {
-    logger.add(
-      new winston.transports.Console({
-        format: winston.format.simple(),
-      }),
-    );
-  }
+if (process.env.NODE_ENV === "production") {
+  transports.push(new AxiomTransport({
+    dataset: "futeboxd-logs",
+    token: process.env.AXIOM_TOKEN as string
+  }))
+} else {
+  transports.push(new winston.transports.Console({
+    format: winston.format.simple(),
+  }))
+}
+
+const logger = winston.createLogger({
+  format: winston.format.json(),
+  defaultMeta: { service: 'football-data-updater' },
+  transports
+});
 
 export default logger;
