9b86c457dad17e25ee2cf23c7f10a36051a11a63
MoDasby <giullianomendes033@gmail.com>
2025-04-14 13:59:30 -0300

    fix: correct status conditions for completed matches in where clause

--- football/internal/match/whereClause.go
+++ football/internal/match/whereClause.go
@@ -45,37 +45,37 @@ func buildWhereClause(options ListMatchOptions) (string, []any) {
 
 	if options.Status == "completed" {
 		appendCondition(
-			fmt.Sprintf("status_name = $%d", counter),
+			fmt.Sprintf("status_name <> $%d", counter),
 			"AND",
 			"STATUS_FIRST_HALF",
 		)
 
 		appendCondition(
-			fmt.Sprintf("status_name = $%d", counter),
+			fmt.Sprintf("status_name <> $%d", counter),
 			"AND",
 			"STATUS_SCHEDULED",
 		)
 
 		appendCondition(
-			fmt.Sprintf("status_name = $%d", counter),
+			fmt.Sprintf("status_name <> $%d", counter),
 			"AND",
 			"STATUS_SECOND_HALF",
 		)
 
 		appendCondition(
-			fmt.Sprintf("status_name = $%d", counter),
+			fmt.Sprintf("status_name <> $%d", counter),
 			"AND",
 			"STATUS_HALFTIME",
 		)
 
 		appendCondition(
-			fmt.Sprintf("status_name = $%d", counter),
+			fmt.Sprintf("status_name <> $%d", counter),
 			"AND",
 			"STATUS_OVERTIME",
 		)
 
 		appendCondition(
-			fmt.Sprintf("status_name = $%d", counter),
+			fmt.Sprintf("status_name <> $%d", counter),
 			"AND",
 			"STATUS_SHOOTOUT",
 		)
