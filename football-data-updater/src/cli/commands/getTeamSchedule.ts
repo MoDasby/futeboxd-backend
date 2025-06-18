13b06af637e87ce5f99276dabd966255cc0cfeb1
MoDasby <giullianomendes033@gmail.com>
2025-06-18 14:22:34 -0300

    refactor: streamline team schedule retrieval and improve error handling in team model

--- football-data-updater/src/cli/commands/getTeamSchedule.ts
+++ football-data-updater/src/cli/commands/getTeamSchedule.ts
@@ -1,62 +1,79 @@
 import loadEnvs from "@/config/load-envs";
 
-loadEnvs()
+loadEnvs();
 
 import db from "@/db";
 import { League, listLeagues } from "@/models/league";
 import { upsertMatch } from "@/models/match";
-import { getMandatoryTeams, insertTeamIfNotExists } from "@/models/team";
+import { insertTeamIfNotExists, listTeams } from "@/models/team";
 import { espnMatchUpdater } from "@/service/match-updater/espn-match-updater";
 import { Command } from "commander";
 
 const program = new Command();
 
 program
-    .name("get team schedule")
-    .option("--from <year>", "Ano de inicio para a busca, ex: 2020")
-    .option("--to <year>", "Ano de fim para a busca, ex: 2023")
-    .option("--source <source>", "Fonte para buscar as partidas, por enquanto apenas espn")
-    .argument("<id>", "id do time no banco de dados")
-    .action(async (id: string = "all") => {
-        const options = program.opts();
+	.name("get team schedule")
+	.option("--from <year>", "Ano de inicio para a busca, ex: 2020")
+	.option("--to <year>", "Ano de fim para a busca, ex: 2023")
+	.option(
+		"--source <source>",
+		"Fonte para buscar as partidas, por enquanto apenas espn"
+	)
+	.argument("<id>", "id do time no banco de dados")
+	.action(async (id: string = "all") => {
+		const options = program.opts();
 
-        const from = Number(options.from)
-        const to = Number(options.to) || new Date().getFullYear()
+		const from = Number(options.from);
+		const to = Number(options.to) || new Date().getFullYear();
 
-        await db.waitForDbReady()
+		await db.waitForDbReady();
 
-        const leagues = await listLeagues()
+		const leagues = await listLeagues();
 
-        if (id === "all") {
-            const teams = await getMandatoryTeams()
+		if (id === "all") {
+			const teams = await listTeams({});
 
-            for (const team of teams) {
-                for (let season = from; season <= to; season++) {
-                    await saveSeasonSchedule(Number.parseInt(team.id), leagues, season)
-                }
-            }
+			for (const team of teams) {
+				for (let season = from; season <= to; season++) {
+					await saveSeasonSchedule(
+						Number.parseInt(team.id),
+						leagues,
+						season
+					);
+				}
+			}
 
-            return
-        }
+			return;
+		}
 
-        for (let season = from; season <= to; season++) {
-            await saveSeasonSchedule(Number.parseInt(id), leagues, season)
-        }
-    })
-    .parse(process.argv);
+		for (let season = from; season <= to; season++) {
+			await saveSeasonSchedule(Number.parseInt(id), leagues, season);
+		}
+	})
+	.parse(process.argv);
 
-async function saveSeasonSchedule(teamId: number, leagues: League[], season: number) {
-    const matches = await espnMatchUpdater.getTeamSchedule(teamId, season, leagues)
+async function saveSeasonSchedule(
+	teamId: number,
+	leagues: League[],
+	season: number
+) {
+	const matches = await espnMatchUpdater.getTeamSchedule(
+		teamId,
+		season,
+		leagues
+	);
 
-    matches.forEach(async match => {
-        const summary = await espnMatchUpdater.getMatchSummary(match.id)
+	matches.forEach(async (match) => {
+		const summary = await espnMatchUpdater.getMatchSummary(match.id);
 
-        match.events = summary;
+		match.events = summary;
 
-        await insertTeamIfNotExists(match.homeCompetitor.team)
-        await insertTeamIfNotExists(match.awayCompetitor.team)
-        await upsertMatch(match);
+		await insertTeamIfNotExists(match.homeCompetitor.team);
+		await insertTeamIfNotExists(match.awayCompetitor.team);
+		await upsertMatch(match);
 
-        console.log(`inserido partida: liga: ${match.league.name} ${match.id}: ${match.homeCompetitor.team.name} ${match.homeCompetitor.score} x ${match.awayCompetitor.score} ${match.awayCompetitor.team.name}`)
-    })
+		console.log(
+			`inserido partida: liga: ${match.league.name} ${match.id}: ${match.homeCompetitor.team.name} ${match.homeCompetitor.score} x ${match.awayCompetitor.score} ${match.awayCompetitor.team.name}`
+		);
+	});
 }
--- football-data-updater/src/models/team.ts
+++ football-data-updater/src/models/team.ts
@@ -2,73 +2,87 @@ import db from "@/db";
 import logger from "@/util/logger";
 
 export type Team = {
-    id: string;
-    name: string;
-    abbreviation: string;
-    color: string;
-    logo: string;
-}
+	id: string;
+	name: string;
+	abbreviation: string;
+	color: string;
+	logo: string;
+};
 
-export async function insertTeamIfNotExists(team: Team) { // TODO precisa baixar a logo e colocar no s3
-    const query = `
+export async function insertTeamIfNotExists(team: Team) {
+	// TODO precisa baixar a logo e colocar no s3
+	const query = `
         INSERT INTO teams(id, name, logo, abbreviation, color)
         VALUES ($1, $2, $3, $4, $5)
         ON CONFLICT(id) DO NOTHING
-    `
+    `;
 
-    try {
-        await db.query(query, [team.id, team.name, team.logo, team.abbreviation, team.color]);
-    } catch (error) {
-        const err = error as Error
+	try {
+		await db.query(query, [
+			team.id,
+			team.name,
+			team.logo,
+			team.abbreviation,
+			team.color,
+		]);
+	} catch (error) {
+		const err = error as Error;
 
-        logger.error("Erro ao inserir time", {
-            originalError: err.message,
-            stackTrace: err.stack
-        });
-    }
+		logger.error("Erro ao inserir time", {
+			originalError: err.message,
+			stackTrace: err.stack,
+		});
+	}
 }
 
 export async function isAnyMandatory(teams: Team[]): Promise<boolean> {
-    const query = `
+	const query = `
         SELECT COALESCE(BOOL_OR(mandatory), FALSE) AS has_mandatory
         FROM teams
         WHERE id IN (${teams.map((_, i) => `$${i + 1}`).join(", ")});
-    `
-
-    try {
-        const result = await db.query<{ has_mandatory: boolean }>(query, teams.map(t => t.id));
-
-        return result.rows[0].has_mandatory
-    } catch (error) {
-        const err = error as Error
-
-        logger.error("Erro ao verificar se um time é mandatório", {
-            originalError: err.message,
-            stackTrace: err.stack
-        });
-    }
-
-    return false;
-}
-
-export async function getMandatoryTeams(): Promise<Team[]> {
-    const query = `
-        SELECT id, name, logo, abbreviation, color
-        FROM teams
-        WHERE mandatory
     `;
 
-    try {
-        const result = await db.query<Team>(query);
-        return result.rows;
-    } catch (error) {
-        const err = error as Error
-        
-        logger.error(`Erro ao buscar times mandatórios`, {
-            originalError: err.message,
-            stackTrace: err.stack
-        });
+	try {
+		const result = await db.query<{ has_mandatory: boolean }>(
+			query,
+			teams.map((t) => t.id)
+		);
 
-        return [];
-    }
+		return result.rows[0].has_mandatory;
+	} catch (error) {
+		const err = error as Error;
+
+		logger.error("Erro ao verificar se um time é mandatório", {
+			originalError: err.message,
+			stackTrace: err.stack,
+		});
+	}
+
+	return false;
+}
+
+export async function listTeams({
+	mandatory,
+}: {
+	mandatory?: boolean;
+}): Promise<Team[]> {
+	const query = `
+        SELECT id, name, logo, abbreviation, color
+        FROM teams
+        ${mandatory ? "WHERE mandatory" : ""}
+    `;
+
+	try {
+		const result = await db.query<Team>(query);
+		return result.rows;
+	} catch (error) {
+		const err = error as Error;
+
+		logger.error(`Erro ao buscar times mandatórios`, {
+			originalError: err.message,
+			stackTrace: err.stack,
+		});
+
+		return [];
+	}
 }
